380596180f6d1df9b505d2599ebb569d511debe7
dongtran <thiendong.iuh@gmail.com>
2023-11-24 22:24:55 +0700

    add code test telegram bot

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const { Telegraf, Markup, Extra } = require("telegraf");
 
 const hostname = '0.0.0.0';
 const port = process.env.PORT || 8080;
@@ -12,3 +13,13 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+const bot = new Telegraf("6811707045:AAG6SB2ry5vbtZEvgWa1mrgeqwG4Z3Sk8dw");
+bot.start((ctx) => ctx.reply("Hello, I'm dev bot~"));
+bot.on("sticker", (ctx) => ctx.reply("👍"));
+
+bot.on("text", async (ctx) => {
+    bot.telegram.sendMessage(ctx.update.message.from.id, ctx.update.message.text);
+});
+
+bot.launch();
